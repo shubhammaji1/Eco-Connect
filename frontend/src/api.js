de50113374041682e8534d9cd8b70bc14631b936
Shubham Maji <122548421+shubhammaji1@users.noreply.github.com>
2025-01-15 02:13:04 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://eco-connect.onrender.com' });
 
 export const signUp = (formData) => API.post('/signup', formData);
 export const logIn = (formData) => API.post('/login', formData);
